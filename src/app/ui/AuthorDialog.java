99736b553f27650785f48bc4f54f7c22f309be28
gapinsky <gapinski02@gmail.com>
2026-06-12 22:13:33 +0200

    author dialog

--- src/app/ui/AuthorDialog.java
+++ src/app/ui/AuthorDialog.java
@@ -1,4 +1,32 @@
 package app.ui;
 
-public class AuthorDialog {
+import app.Window;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AuthorDialog extends JDialog {
+
+    public AuthorDialog(Window owner) {
+        super(owner, "O autorze", true);
+
+        setSize(350, 200);
+        setLocationRelativeTo(owner);
+        setLayout(new BorderLayout());
+
+        JLabel authorInfo = new JLabel(
+                "<html><center>" +
+                        "<h2>Projekt Java - Swing</h2>" +
+                        "<p>Autor: Antoni Gapiński</p>" +
+                        "<p>Politechnika Koszalińska 2026</p>" +
+                        "</center></html>",
+                SwingConstants.CENTER
+        );
+
+        JButton closeButton = new JButton("Zamknij");
+        closeButton.addActionListener(e -> dispose());
+
+        add(authorInfo, BorderLayout.CENTER);
+        add(closeButton, BorderLayout.SOUTH);
+    }
 }
