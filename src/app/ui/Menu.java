e62e7b52509da93e7563266af882ccf378fe45d6
gapinsky <gapinski02@gmail.com>
2026-06-12 22:10:20 +0200

    menu UI class

--- src/app/ui/Menu.java
+++ src/app/ui/Menu.java
@@ -1,4 +1,19 @@
 package app.ui;
 
 public class Menu {
+    public void show() {
+        System.out.println("=== MENU ===");
+        System.out.println("1. Status");
+        System.out.println("2. Narzędzia");
+        System.out.println("3. Autor");
+        System.out.println("0. Wyjście");
+    }
+
+    public void showInvalidOption() {
+        System.out.println("Nieprawidłowa opcja. Spróbuj ponownie.");
+    }
+
+    public void showExitMessage() {
+        System.out.println("Zamykanie aplikacji...");
+    }
 }
